21d6dc8f5eae46acf3db6b261e2a5337acdd1d8c
kaiser <jkaisermp@protonmail.com>
2025-06-08 21:06:42 -0300

    Progress on student list

--- Student list/README.md
+++
@@ -1,5 +0,0 @@
-> Log in via Google - to GDB Online.
-
-Compile: gcc main.c
-
-Run: ./a.out
--- Student list/main.c
+++ Student list/main.c
@@ -1,84 +1,127 @@
-/*
-    BCC-PRE 2025.1
-    Exercício de Busca Binária
-    Data: 22/maio/2025
-
-    Aluno(a): Júlio César de Amoraes Pinheiro
-*/
-
 #include <stdio.h>
-// WARNING: Arrays can't be passed to functions - they only receive a pointer to first element
-int query_vector(int chosen_vector[], int size, int query);
-int sort_vector(int chosen_vector[], int size);
-int print_vector(int chosen_vector[], int size);
-// Vectors are like variables. Declare them the same way on the parameters, plus []
+#include <string.h>
+
+// PENDING NOTES IN SKETCH
+
+
+
+
+
+
+
+
+// Gosto de manter minhas anotações em markdown, mas quando se trata de código
+// Gosto de manter como comentários, no meio do código e em seções dedicadas
+// Por isso a estrutura um pouco "bloated" - como é um código de estudo
+
+
+// --- Requirements
+// Aluno struct
+// with nome, numMatricula e nota
+// Note: Using typedef is the same thing as this, but acting as/giving an alias to the datatype
+
+#define NUMBER 0
+#define NAME 1
+
+struct PassingStruct {
+    char passing_nome[50];
+    int passing_numMatricula;
+    int passing_nota;
+};
+
+struct Aluno { // New data type grouping multiple variables
+    char nome[50];
+    int numMatricula;
+    int nota;
+    struct PassingStruct passingList; // nests the struct as a list inside Aluno
+    // To access PassingStruct fields: pessoaVariable.endereco.rua, pessoaVariable.endereco.numero...
+};
+
+struct Aluno indexAlunos[100]; // Same as adding only the last item in the end of the struct definition
+
+// --- Requirements
+// On main, create vectors: index, name, numMatricula, nota
+// Prompt user for these
+// Print name of those who've failed the class (nota < 6)
+// --- Function mapping
+// userPrompt           needs: struct
+// filterStudents       needs: each struct      returns: passing (which is: a struct)
+// printPassing         needs: passing
+
+void userPrompt(void);
+int filterStudents(int passing);
+int validateInput(void*, int type);
+void printPassing();
+
+
+// declare passing;
+
+// --- --- --- --- --- --- --- --- --- --- --- --- Functions
 
 int main() {
-  int option = 0;
-  int query = 0;
-  int dataset[] = {13, 1230, 1203, 1230, 1419823, 910, 1, 2, 3, 10};
-  int boundaries = sizeof(dataset) / sizeof(int); // Size of vector / Size of data-type/element = Number of elements
-  printf("Escolha uma função (digite o número): \n");
-  printf("Opções          Descrição \n");
-  printf("0               Busca Binária \n");
-  scanf("%d", &option);
-  switch (option) {
-  case 0: {
-    sort_vector(dataset, boundaries); // Using variables of said name as parameters
-    printf("Escolha um número: ");
-    scanf("%d", &query);
-    query_vector(dataset, boundaries, query);
-    break;
-  }
-  }
-  return 0;
+    // Find a ((n) efficient) way to run tests through the functions, to pin down problems
+    // Begin studying and exploring "testing" and "debugging" and part of coding
+    // ...not something you do when things go wrong, but by default - as they always do.
+    userPrompt();
+    filterStudents(passing);
+    printPassing();
 }
 
-int query_vector(int chosen_vector[], int size, int query) {
-  int numberOfVectorElements = size;
-  int iterations = 1;
-  int start = 0;
-  int end = size - 1;
-  while (start <= end) {
-    int middle = (start + end) / 2;
-    if (chosen_vector[middle] == query) {
-      printf(" Valor: %d \n Encontrado na posição: %d \n Após: %d iterações", query, middle, iterations);
-      return 1;
-    } else if (chosen_vector[middle] > query) {
-      // ignore right of the list, then continue search
-      end = middle - 1; // Must ignore the tested case for this to progress
-    } else if (chosen_vector[middle] < query) {
-      // ignore left of the list, then continue search
-      start = middle + 1;
+// Crie função que valída input
+// recebe dois parametros
+// 1 do dado que vai validar
+// ... Isso cria um problema... Tipos de dados diferentes
+// 2 para o tipo de validação (senha, nome, número)
+
+void validadeInput(void*, int type) {
+    if (type == NUMBER) {
+        // Cast void* to int
+
+    } else if (type == NAME) {
+        // Cast void* to str
+
     }
-    iterations++;
-  }
-  printf("Valor não encontrado\n");
-  // Show index of where the number was, or message that it was not there, and how many iterations it took
-  return 0;
 }
 
-int sort_vector(int chosen_vector[], int size) {
-  int aux = 0;
-  for (int i = 0; i < size - 1; i++) {
-    if (chosen_vector[i] > chosen_vector[i + 1]) {
-      aux = chosen_vector[i];
-      chosen_vector[i] = chosen_vector[i + 1];
-      chosen_vector[i + 1] = aux;
-      i = -1; // This will restart to 0 once there's a change
+void userPrompt() {
+    // --- Map
+    // Prompt how many students to add          // In the future, adapt it to add from the last addition, instead of overwriting from the first position
+    // Prompt Loop
+    //      nome,   numMatricula,   nota
+    //      add all of this to index
+    int numOfStudents = 0;
+    printf("\n");
+    printf("Quantos alunos você quer adicionar? ");
+    scanf("%d", &numOfStudents);
+    for (int i = 0; i < numOfStudents; i++) {
+        printf("        (Você pode voltar a qualquer momento, digitando \"voltar\" num campo) \n");
+        printf("Nome do %dº aluno: ", i);
+        scanf("%s", &Aluno.nome[50]);
+        if (Aluno.nome == "voltar" || Aluno.numMatricula == "voltar" || Aluno.nota == "voltar") {
+            i--;
+            continue; // Skips the rest of the iteration
+        }
+        printf("Número da matrícula de %d: ", Aluno.numMatricula);
+        scanf("%d", &Aluno.numMatricula);
+        if (Aluno.nome == "voltar" || Aluno.numMatricula == "voltar" || Aluno.nota == "voltar") {
+            i--;
+            continue;
+        }
+        printf("Qual foi a nota de %s? ", Aluno.nome);
+        scanf("%d", &Aluno.nota);
+        if (Aluno.nome == "voltar" || Aluno.numMatricula == "voltar" || Aluno.nota == "voltar") {
+            i--;
+            continue;
+        }
+        // Num programa real precisaria tratar a excessão de dados impróprios - como adicionando números nas strings de nome, ou o contrário
     }
-  }
-  print_vector(chosen_vector, size);
-  return 0;
 }
 
-int print_vector(int chosen_vector[], int size) {
-  int index = 0;
-  int counter = 0;
-  printf("—————————————— \n");
-  for (int i = 0; i < size; i++) {
-    printf("%d:    %d \n", counter, chosen_vector[i]);
-    counter++;
-  }
-  return 0;
+int filterStudents() {
+    // --- Map
+    return passing;
+}
+
+void printPassing() {
+    // --- Map
 }
